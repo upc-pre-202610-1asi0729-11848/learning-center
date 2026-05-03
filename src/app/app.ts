7cfc9f5d5d073f5ee43860b17564f56be18cc2ba
Angel Velasquez <pcisavel@upc.edu.pe>
2026-05-02 21:57:11 -0500

    feat(app): integrate translation service and update app structure.

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,30 @@
-import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import {Component, inject, signal} from '@angular/core';
+import {TranslateService} from "@ngx-translate/core";
+import {Layout} from "./shared/presentation/components/layout/layout";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [Layout],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
+  /**
+   * Signal for the application title.
+   */
   protected readonly title = signal('learning-center');
+
+  /**
+   * Translation service instance.
+   */
+  private translate: TranslateService;
+
+  /**
+   * Creates an instance of App and sets up translation.
+   */
+  constructor() {
+    this.translate = inject(TranslateService);
+    this.translate.addLangs(['en', 'es']);
+    this.translate.use('en');
+  }
 }
