f969e7492361c2d4a649afb1fdb99c8ac51b285f
Angel Velasquez <pcisavel@upc.edu.pe>
2026-05-02 21:56:44 -0500

    feat(category): implement category form with validation and submission logic.

--- src/app/learning/presentation/views/category-form/category-form.ts
+++ src/app/learning/presentation/views/category-form/category-form.ts
@@ -1,11 +1,23 @@
 import {Component, inject} from '@angular/core';
-import {FormBuilder} from '@angular/forms';
+import {FormBuilder, FormControl, ReactiveFormsModule, Validators} from '@angular/forms';
 import {ActivatedRoute, Router} from '@angular/router';
 import {LearningStore} from '../../../application/learning.store';
+import {Category} from "../../../domain/model/category.entity";
+import {MatError, MatFormField, MatInput, MatLabel} from "@angular/material/input";
+import {TranslatePipe} from "@ngx-translate/core";
+import {MatButton} from "@angular/material/button";
 
 @Component({
   selector: 'app-category-form',
-  imports: [],
+  imports: [
+    ReactiveFormsModule,
+    MatFormField,
+    MatLabel,
+    TranslatePipe,
+    MatError,
+    MatInput,
+    MatButton
+  ],
   templateUrl: './category-form.html',
   styleUrl: './category-form.css',
 })
@@ -14,4 +26,57 @@ export class CategoryForm {
   private route = inject(ActivatedRoute);
   private router = inject(Router);
   private store = inject(LearningStore);
+  /**
+   * Form group for the category form.
+   */
+  form = this.fb.group({
+    name: new FormControl<string>('', { nonNullable: true, validators: [Validators.required] })
+  });
+
+  /**
+   * Indicates if the form is in edit mode.
+   */
+  isEdit = false;
+
+  /**
+   * The ID of the category being edited, or null for new categories.
+   */
+  categoryId: number | null = null;
+
+  /**
+   * Creates an instance of CategoryForm and initializes the form based on route parameters.
+   */
+  constructor() {
+    this.route.params.subscribe(params => {
+      this.categoryId = params['id'] ? +params['id'] : null;
+      this.isEdit = !!this.categoryId;
+      if (this.isEdit && this.categoryId) {
+        let id = this.categoryId;
+        const category = this.store.getCategoryById(id)();
+        if (category) {
+          this.form.patchValue({ name: category.name });
+        }
+      }
+    });
+  }
+
+  /**
+   * Submits the form to create or update the category.
+   */
+  submit() {
+    if (this.form.invalid) return;
+
+    const category: Category = new Category({
+      id: this.categoryId ?? 0,
+      name: this.form.value.name!
+    });
+
+    if (this.isEdit) {
+      this.store.updateCategory(category);
+    } else {
+      this.store.addCategory(category);
+    }
+
+    this.router.navigate(['learning/categories']).then();
+  }
 }
